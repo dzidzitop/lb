042383bd120b806898b221b616ea0442ad61a62e
Dźmitry Laŭčuk <dzidzitop@vfemail.net>
2022-08-31 22:01:44 +0200

    Step 3: LoadBalancer#get() added as a proxy to Provider#get() with random selection among provider instances.

--- src/main/java/dzmitry/loadbalancer/LoadBalancer.java
+++ src/main/java/dzmitry/loadbalancer/LoadBalancer.java
@@ -1,25 +1,38 @@
 package dzmitry.loadbalancer;
 
 import java.util.Objects;
+import java.util.Random;
 
-public class LoadBalancer<T>
+public class LoadBalancer
 {
     private static final int MAX_SIZE = 10;
     
-    private final T[] instances;
-    private int size; // eventually consistent with instances.
+    private final Provider[] instances;
+    private int size;
+    private final Random rnd;
     
     public LoadBalancer()
     {
-        instances = (T[]) new Object[MAX_SIZE];
+        instances = new Provider[MAX_SIZE];
+        rnd = new Random(System.nanoTime());
     }
     
-    public synchronized void register(final T instance)
+    public synchronized void register(final Provider instance)
     {
         Objects.requireNonNull(instance);
         if (size == MAX_SIZE) {
             throw new IllegalStateException("Max size already reached.");
         }
         instances[size++] = instance;
+    }
+    
+    public String get()
+    {
+        final Provider provider;
+        synchronized (this) {
+            final int idx = rnd.nextInt(size);
+            provider = instances[idx];
+        }
+        return provider.get();
     }
 }
